c6aeabb8df739272f241e0db45231e5f3414883e
jxch <qianbuhan@proton.me>
2023-12-20 04:06:37 +0800

    监听全局快捷键

--- src/main/java/org/jxch/copyplus/copyplus/MainApplication.java
+++ src/main/java/org/jxch/copyplus/copyplus/MainApplication.java
@@ -1,23 +1,34 @@
 package org.jxch.copyplus.copyplus;
 
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
-public class MainApplication extends Application {
+@Slf4j
+public class MainApplication extends Application implements NativeKeyListener {
+    private static final String MAIN_FXML = "main-view.fxml";
+    private static final String TITLE = "Copy Plus";
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+    public void start(@NonNull Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(MAIN_FXML));
         Scene scene = new Scene(fxmlLoader.load(), 500, 800);
-        stage.setTitle("Copy Plus");
+        stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
+
+        GlobalKeyListener.registerNativeHook();
+        stage.setOnCloseRequest((event) -> GlobalKeyListener.unregisterNativeHook());
     }
 
     public static void main(String[] args) {
         launch();
     }
+
 }
